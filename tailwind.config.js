ecf0cc4417993b60a0b5262b36d1e722c034ee26
laiso <aloisleroy10@gmail.com>
2025-04-04 15:09:17 +0200

    custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,6 @@ export default {
     content: ["./src/**/*.{svelte,js,ts}"],
     theme: {
       extend: {
-        colors: {
-          custom: {
-            black: "#000000",
-            white: "#FFFFFF",
-            green: "#00FF00",
-            blue: "#0000FF",
-          }
-        },
       },
     },
     plugins: [],
