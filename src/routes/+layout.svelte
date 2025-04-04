0bd1e066f5c7f3380f976c8eb98b57e7d963bbec
laiso <aloisleroy10@gmail.com>
2025-04-04 13:49:40 +0200

    Move font import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,4 @@
 	let { children } = $props();
 </script>
 
-<style>
-	@import url('https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,100..900;1,100..900&display=swap');
-</style>
-
 {@render children()}
