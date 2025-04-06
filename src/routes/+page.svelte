9fc84d53ecd0899481d8650f0067e99de8dad9ad
laiso <aloisleroy10@gmail.com>
2025-04-06 22:23:50 +0200

    adding footer + about

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,14 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Hero from "$lib/components/Hero.svelte";
 	import Projects from '$lib/components/Projects.svelte';
+	import About from '$lib/components/About.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <Navbar />
+
 <Hero />
 <Projects />
-<slot />
+<About />
+
+<Footer />
