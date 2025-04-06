a17aa68c55d7bd6c2ceac029be07180290025a30
laiso <aloisleroy10@gmail.com>
2025-04-06 22:22:00 +0200

    project -> projects

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,7 +2,7 @@
 	let isOpen = false;
 </script>
 
-<nav class="fixed z-50 flex h-20 w-screen flex-row items-center justify-between bg-[linear-gradient(180deg,_black_15%,_transparent_100%)] px-4">
+<nav class="fixed z-50 flex h-20 w-screen flex-row items-center justify-between bg-gradient-to-b from-black via-black to-transparent px-4">
 	<div class="h-full flex items-center">
 		<a href="/" draggable="false" class="aspect-square max-h-full">
 			<img src="/images/logo.svg" alt="Asterilit logo" draggable="false" class="h-full select-none" />
@@ -11,8 +11,8 @@
 
 	<ul class="desktop-nav hidden md:flex justify-around h-full w-1/2 items-center space-x-8">
 		<li>
-            <a href="#project" class="text-white sm:text-lg lg:text-xl xl:text-2xl">
-                project
+            <a href="#projects" class="text-white sm:text-lg lg:text-xl xl:text-2xl">
+                projects
             </a>
         </li>
 		<li>
@@ -42,10 +42,10 @@
 	<ul class="md:hidden fixed pt-20 z-40 w-full flex flex-col items-center gap-6 bg-black bg-opacity-80 py-6">
 		<li>
             <a 
-                href="#project" 
+                href="#projects" 
                 class="text-white text-xl" 
                 on:click={() => (isOpen = false)}>
-                project</a
+                projects</a
                 >
         </li>
 		<li>
