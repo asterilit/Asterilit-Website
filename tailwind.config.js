c4e8b9cab8a9a4378467b8115ed2545db9765394
laiso <aloisleroy10@gmail.com>
2025-04-21 00:48:55 +0200

    pnpm up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{svelte,js,ts}"],
-    theme: {
-      extend: {
-      },
-    },
-    plugins: [],
-  };
-  
+	content: {
+		files: ['./src/**/*.{svelte,js,ts}'],
+	},
+	theme: {
+		extend: {
+		}
+	},
+	plugins: []
+};
