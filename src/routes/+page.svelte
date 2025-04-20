184e1565c7e21fca4b7d3339d898d5bd7eee244f
laiso <aloisleroy10@gmail.com>
2025-04-21 00:50:36 +0200

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Navbar from '$lib/components/Navbar.svelte';
-	import Hero from "$lib/components/Hero.svelte";
+	import Hero from '$lib/components/Hero.svelte';
 	import Projects from '$lib/components/Projects.svelte';
 	import About from '$lib/components/About.svelte';
 	import Footer from '$lib/components/Footer.svelte';
